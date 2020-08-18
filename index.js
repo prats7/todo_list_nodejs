2a28830fa190dad7af1519326077e379fb69f023
Pratyush Vishal <pratyushvishal04@gmail.com>
2020-08-18 17:02:13 +0530

    setup views

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 8000;
 
+//set ejs
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname, 'views'));
+
 //use express router
 app.use('/',require('./routes'));
+
+app.get('/', (req,res) => {
+    return res.render('home', {title: "TODO List"});
+});
 
 app.listen(port, function(err){
     if(err){
