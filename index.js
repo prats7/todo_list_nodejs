1d20cfbb07628009f51805c1d3987a45dbcc379a
Pratyush Vishal <pratyushvishal04@gmail.com>
2020-08-18 20:02:54 +0530

    updated views

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+lconst express = require('express');
 const path = require('path');
 const app = express();
 const port = 8000;
@@ -7,11 +7,25 @@ const port = 8000;
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname, 'views'));
 
+var todoList = [
+    {
+        description: "vegetables",
+        category: "personal",
+    },
+    {
+        description: "vegetables",
+        category: "personal",
+    }
+]
+
 //use express router
 app.use('/',require('./routes'));
 
 app.get('/', (req,res) => {
-    return res.render('home', {title: "TODO List"});
+    return res.render('home', {
+        title: "TODO List",
+        todo_list: todoList
+    });
 });
 
 app.listen(port, function(err){
